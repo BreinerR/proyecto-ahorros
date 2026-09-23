8a8db0d8aa2f7cf6cf10348e708e67371767ffbb
BreinerR <breineralexander1800@gmail.com>
2026-09-23 11:18:18 -0500

    Actualizacion de URL y API Key en supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza TU_PROJECT_URL con la URL de tu proyecto en Supabase (está un poco más arriba en esa misma página)
-const SUPABASE_URL = 'sb_publishable_gLsYSSQJSvCmuLd6ZPTf3A_iekjag78';
+const SUPABASE_URL = 'https://zduuszobtsusrvmicbvh.supabase.co/rest/v1/';
 
 // Pega aquí la clave que acabas de copiar
 const SUPABASE_ANON_KEY = 'sb_secret_5FJJt1ixyX0-I-lA8e5GXw_rPnk8LXE';
